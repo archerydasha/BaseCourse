806e1cabe2683b21796564ad68f914d2328177a3
Daria Shcherbyna <daria.shcherbyna@db.com>
2014-03-12 13:52:39 +0200

    Initial version of logger]

--- actions/src/main/java/com/basecourse/actions/Frontend.java
+++ actions/src/main/java/com/basecourse/actions/Frontend.java
@@ -8,33 +8,37 @@ import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Created by dshcherbyna on 26.02.14.
  */
 public class Frontend {
     private Set<Action> actions;
+    private Logger LOG = Logger.getLogger(Frontend.class);
 
     public Frontend() {
         Injector injector = DI.getInjector();
         actions = injector.getInstance(Key.get(new TypeLiteral<Set<Action>>() {
         }));
+       PropertyConfigurator.configure(Frontend.class.getResourceAsStream("/com/basecourse/actions/log4j.properties"));
     }
 
     public void processAction(String params) {
+        LOG.info("Frontend.processAction function called with parameter " + params);
         String[] paramsArray = Iterables.toArray(Splitter.on(',').trimResults().split(params), String.class);
         String eventType = getParamValue(paramsArray[0]);
         String fileName = getParamValue(paramsArray[1]);
+        LOG.info("Properties are: " + eventType + ", " + fileName);
         Properties properties = new Properties(EventType.valueOf(eventType), fileName);
         processAction(properties);
     }
 
     private void processAction(Properties properties) {
         Action action = findNeededAction(properties.getEventType());
+        LOG.info("Action to be used is: " + action.getClass());
         action.processEvent(properties);
     }
 
